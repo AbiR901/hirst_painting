93f95b86c59e6efce4758c53383ab94c89949a56
Abir <official.abir009@gmail.com>
2022-06-16 00:48:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+'''Program to extract different colors from a given image'''
+import colorgram
+
+colors = colorgram.extract('image.jpg', 10)
+
+rgb_colors = []
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
+
